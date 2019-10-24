d31a6de3829ac0335a196464aaaa6385b9406073
Soufian ELHABIB <elhabib.soufian@gmail.com>
2019-10-24 15:30:39 +0100

    Add Readme file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Instagrame
+                    Instagram
                 </div>
 
                 <div class="links">
